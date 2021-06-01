ddc895ff33ded2fa5e432b33d543b8691161ff92
Ash-jo121 <47566176+Ash-jo121@users.noreply.github.com>
2021-06-01 19:14:23 +0530

    First Commit

---
+++ contracts/Lottery.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    string public manager;
+    address[] public players;
+
+    function Lottery() public {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > 0.01 ether);
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public payable restricted {
+        uint256 index = random() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address[]) {
+        return players;
+    }
+}
+
+// https://goerli.infura.io/v3/32c7b78d3447433a8488396c66051605
+
+// merry film assist inmate disagree cheap sketch friend margin dice license mention
